2d3cd1c3289c22588ccd4ff6d893e77ea3abb53a
HubertCK <ak47@t.pl>
2014-04-13 17:28:00 +0200

    Dodano SWC
    Dodano stronę SWC - szkielet

---
+++ swc/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SWC - StickWorld Corporation</title>
+<meta charset="UTF-8">
+<link href="img/style.css" rel="stylesheet" rev="stylesheet">
+</head>
+<body>
+
+<div id="topbar">
+	<div id="all">
+		<div id="logo"><img src="img/swc.png"></div>
+		<div id="menu">
+			<ul>
+				<a href="#"><li>SWC</li></a>
+				<a href="#"><li>Mobi</li></a>
+				<a href="#"><li>Codesty</li></a>
+				<a href="#"><li>Ection</li></a>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<div id="content">
+	<div id="all">
+		<div id="menuc">
+			<ul>
+				<a href="#"><li>Strona Główna</li></a>
+				<a href="#"><li>O nas</li></a>
+				<a href="#"><li>Produkty</li></a>
+				<a href="#"><li>Kariera</li></a>
+				<a href="#"><li>Aktualności</li></a>
+				<a href="#"><li>Kontakt</li></a>
+			</ul>
+		</div>
+		<div id="text">
+			<h1>STRONA W TRAKCIE BUDOWY</h1>
+			<p>Zajżyj ponownie później.</p>
+		</div>
+		<div id=""></div>
+		<div id=""></div>
+		<div id=""></div>
+	</div>
+</div>
+
+
+
+
+
+
+
+<div id=""></div>
+<div id="clear"></div>
+
+</body>
+</html>
